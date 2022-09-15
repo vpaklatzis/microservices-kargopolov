2d0660e92b1bdebbf9b788b1e409b68a4d68f4b7
vpaklatzis <vpaklatzis@github.com>
2022-09-15 14:25:48 +0300

    Initialized and configured users service

---
+++ users-service/src/main/java/com/demo/users/controllers/UsersController.java
@@ -0,0 +1,16 @@
+package com.demo.users.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UsersController {
+
+    @GetMapping("/status/check")
+    public String getStatus() {
+        return "Working";
+    }
+}
